613d21cd8960f6e40f1eff4391cfb36a1d36a0f8
chininha-03 <tatazinhoow@gmail.com>
2023-08-10 08:56:19 -0300

    Mais um código para a img ficar alterando.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ var toogleMode = () => {
 
     html.classList.toggle("light")
 
-    const isLight = html.classList.contains("light")
+    const isLight = html.classList.contains("light");
+    const srcValue = isLight ? "../img/eudnv.png" : "../img/eu.jpeg";
+
+    document.querySelector("#profile img").setAttribute("src",srcValue)
 
     console.log(isLight)
 
