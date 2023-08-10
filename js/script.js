64ec222693eb45899f95277a3c493c11689277c1
chininha-03 <tatazinhoow@gmail.com>
2023-08-10 10:31:16 -0300

    oi hihii

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@ var toogleMode = () => {
 
     const isLight = html.classList.contains("light");
     const srcValue = isLight ? "../img/eudnv.png" : "../img/eu.jpeg";
+    const altValue = isLight ? "Imagem de uma pessoa bonita na praia, ao lado de uma placa de turismo explicando qual é o nome dos locais por perto" : "Imagem de perfil de uma pessoa bonita na praia"
 
-    document.querySelector("#profile img").setAttribute("src",srcValue)
+    document.querySelector("#profile img").setAttribute("src",srcValue);
 
+    document.querySelector("#profile img").setAttribute("alt",altValue);
+    
     console.log(isLight)
 
 }
